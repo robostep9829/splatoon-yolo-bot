0590442fac92cc6aeb8c2f8b19c7f6116739de05
robostep9829 <robostep9829@gmail.com>
2023-06-06 23:15:39 +0300

    Back to numpy array because needed for sorting. Now it aims at closest inkling to the center, excluding your inkling

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ w = 1280  # set this
 h = 720  # set this
 
 hwnd = None
-hwnd = win32gui.FindWindow(None, 'Ryujinx 1.1.747 - Splatoon 2 v5.5.1 (0100F8F0000A2000) (64-bit)')
+hwnd = win32gui.FindWindow(None, 'Ryujinx 1.1.869 - Splatoon 2 v5.5.1 (0100F8F0000A2000) (64-bit)')
 left, top, right, bottom = win32gui.GetWindowRect(hwnd)
 
 camera = dxcam.create(output_idx=0, output_color="BGR")
@@ -23,7 +23,7 @@ camera.start(video_mode=True, region=(left, top, right, bottom))
 session = ort.InferenceSession('best.onnx', providers=['DmlExecutionProvider'])
 
 
-def screenshot(result_raw):
+def screenshot(result_raw) -> None:
     while True:
         image = cv2.resize(camera.get_latest_frame(), (640, 640))
         image = torch.tensor(image.transpose((2, 0, 1)), dtype=torch.float32) / 255.0
@@ -36,23 +36,31 @@ def screenshot(result_raw):
         # readData(result_raw)
 
 
-def readData(result_raw):
+def readData(result_raw) -> None:
     while True:
         # boxes = result_raw['main']
-        boxes = set(map(tuple,process3(result_raw['main'])))
+        ## boxes = set(map(tuple, process3(result_raw['main'])))
+        boxes = process3(result_raw['main'])
         img = np.zeros(shape=[640, 640, 3], dtype=np.uint8)
+        cv2.circle(img, (320, 320), 6, [255, 255, 255], 4)
+        cv2.circle(img, (320, 455), 6, [200, 200, 200], 4)
         if len(boxes) > 0:
-            for index in boxes:
-                cv2.circle(img, (int(index[0]), int(index[1])), int(index[2]), [255, 0, 0], 4)
+            for u, index in enumerate(boxes):
+                color = [255, 0, 0]
+                if u == 0:
+                    color = [255, 255, 0]
+                cv2.circle(img, (int(index[0]), int(index[1])), int(index[2]), color, 4)
         cv2.imshow('image', img)
         cv2.waitKey(1)
         if is_caps_lock_on() and len(boxes) > 0:
-            for index in boxes:
+            #for index in boxes:
                 # print(index)
                 # box = np.array(index) / [640, 640, 640, 640]
                 # print(box)
-                if not (math.isclose(index[0]/640, 0.5, abs_tol=0.05) and math.isclose(index[1]/640, 0.73, abs_tol=0.08)):
-                    keyDirection(index[0]/640, index[1]/640, 0.05)
+                #if not (math.isclose(index[0]/640, 0.5, abs_tol=0.05) and math.isclose(index[1]/640, 0.73, abs_tol=0.08)):
+                    # keyDirection(index[0]/640, index[1]/640, 0.05)
+                    # continue
+            keyDirection(boxes[0][0]/640, boxes[0][1]/640, 0.05)
             time.sleep(0.01)
 
 
@@ -65,7 +73,7 @@ def readData(result_raw):
         # print('done')
 
 
-def keyDirection(player_x, player_y, sleep):
+def keyDirection(player_x: float, player_y: float, sleep: float) -> None:
     if -0.5 < player_x - 0.5 < -0.02:
         # print('left')
         if -0.5 < player_y - 0.5 < -0.02:
@@ -100,18 +108,22 @@ def keyDirection(player_x, player_y, sleep):
             win32api.keybd_event(0x4C, 0, win32con.KEYEVENTF_KEYUP, 0)  # L
 
 
-def is_caps_lock_on():
+def is_caps_lock_on() -> bool:
     # Get the state of the caps lock key
     state = win32api.GetKeyState(win32con.VK_CAPITAL)
     # Check if the high-order bit is 1 (caps lock is on)
     return state & 0x0001 != 0
 
 
-def process3(results):
+def process3(results: np.ndarray) -> np.ndarray:
     boxes, scores = results[:4, :].T.astype(int), results[4, :]
     indices = cv2.dnn.NMSBoxes(boxes, scores, 0.4, 0.5)
     if len(indices) > 0:
-        return boxes[indices]
+        retval = boxes[indices]
+        index_to_remove = np.argmin(np.linalg.norm(retval[:, :2] - [320, 455], axis=1))
+        retval = np.delete(retval, index_to_remove, axis=0)
+        distances = np.linalg.norm(retval[:, :2] - [320, 320], axis=1)
+        return retval[np.argsort(distances)]
     else:
         return []
 
--- train_on_video.py
+++ train_on_video.py
@@ -1,32 +1,42 @@
 import os
 import cv2
+import numpy
 import numpy as np
 import onnxruntime as ort
+import subprocess
+import shutil
 
+filename = 'Splatoon 3 Online Turf Wars ｜ Livestream [No Commentary] [_kSw_heA4Kk].webm'
+directory = 'video3'
 
 def process3(results):
     boxes, scores = results[:4, :].T.astype(int), results[4, :]
     indices = cv2.dnn.NMSBoxes(boxes.tolist(), scores.tolist(), 0.4, 0.5)
     if len(indices) > 0:
-        return boxes[indices]
+        return set(map(tuple, boxes[indices]))
     else:
         return []
 
 
 session = ort.InferenceSession('best.onnx', providers=['DmlExecutionProvider'])
-for file in os.listdir('Videos/frames/'):
-    image = cv2.imread('Videos/frames/'+file)
+frames = []
+for file in os.listdir('Videos/'+directory):
+    image = cv2.imread('Videos/'+directory+'/'+file)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (640, 640)).astype(np.float32) / 255.0
     image = np.transpose(image, (2, 0, 1))
     image = np.expand_dims(image, axis=0)
+    print(str(file)+' ', end='\r', flush=True)
     boxes = session.run(None, {'images': image})[0][0]
     boxes = process3(boxes)
-    if len(boxes) > 0:
+    if len(boxes) > 1:
+        frames.append(file[:-4])
         # with open('Videos/frames/txt/'+file[:-4]+'.txt')
         # print('Videos/frames/txt/'+file[:-4]+'.txt')
-        with open('Videos/frames/txt/'+file[:-4]+'.txt', 'w') as text:
+        with open('Videos/'+directory+'/txt/'+file[:-4]+'.txt', 'w') as text:
             for index in boxes:
-                result = np.around(index / [640, 640, 640, 640], decimals=6)
+                result = np.around(numpy.array(index) / [640, 640, 640, 640], decimals=6)
                 text.write(f'0 {" ".join(str(i) for i in result)}\n')
-
+        #command = ['ffmpeg', '-i', 'Videos/'+filename, '-y', '-vf', f'select=\'eq(n\,{file[:-4]}\')', '-vframes', '1', f"{f'Videos/{directory}/extracted'}/{file[:-4]}.png"]
+        #subprocess.call(command, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        shutil.copy('Videos/'+directory+str(file),'Videos/'+directory+'/'+'extracted/'+str(file))
